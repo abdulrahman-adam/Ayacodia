6a4ebc4b7e3179a5619ab6f8bc5d2e4ffbcb5fd0
Abdulrahman <abdulrahman939291@gmail.com>
2026-05-17 19:28:08 +0200

    Add mobile accordion animation for subsystem cards with smooth expand/collapse interaction

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import CategoryDrillDown from '../../components/header/Header'
 import Header from '../../components/header/Header'
 import PartenaireList from '../../components/partenaireList/PartenaireList'
 import Accordion from '../../components/accordion/Accordion'
+import ServicesData from '../../components/serviceData/ServiceData'
 
 
 
@@ -12,6 +13,7 @@ const Home = () => {
     <div className='bg-gray-100'>
       {/* <Hero /> */}
       <Header />
+      <ServicesData />
       <Accordion />
       <PartenaireList />  
     </div>
